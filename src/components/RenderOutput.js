fe13d69efdec482d7d59fa911ad5cb98534d4c07
Carlos E <58950500+CarlosElizalde98@users.noreply.github.com>
2022-11-17 08:34:55 -0600

    Made CSS styling changes to submitted form page

--- src/components/RenderOutput.js
+++ src/components/RenderOutput.js
@@ -1,63 +1,63 @@
 import React, { useState } from "react";
 
-function RenderOutput({items, handleChange, name}) {
-    const [editing, setEdit] = useState(true)
+function RenderOutput({ items, handleChange, name }) {
+  const [editing, setEdit] = useState(true);
 
-        this.state = {
-            editing: true,
-        }
-    }
-    handleChanges = (e) => {
-        const obj = e.target.id;
-        const value = e.target.value;
-        handleChange(name, obj, value)
-        
-        }
-    const handleSave = () => {
-        setEdit( editing => !editing
-        )
-    }
-    
-    const renderItems = (props) => {
-        const items = props;
-        console.log(items)
-        let objects = Object.keys(items).map((key) => [`${key}: ${items[key]}`])
-        console.log(objects)
-        let displays = objects.map((item) => {
-            return <div key={item} className="resume-item">
-                <p>{item}</p>
-                </div>
-        })
-        return (
-            <div className="resume-display">
-                {displays}
-                  <button onClick={handleSave}>Edit</button>
-            </div>
-        );
-    }
+  const handleChanges = (e) => {
+    const obj = e.target.id;
+    const value = e.target.value;
+    handleChange(name, obj, value);
+  };
+  const handleSave = () => {
+    setEdit((editing) => !editing);
+  };
 
-    const editItems = (props) => {
-        const items = props;
-        let objects = 
-            Object.keys(items).map((key) => 
-                <input type="text" 
-                defaultValue={items[key]} 
-                key={key}
-                id={key}
-                onChange={(e) => handleChanges(e)}
-             />
-        )
-        return (
-            <div className="resume-display">
-                {objects}
-                <button onClick={handleSave}>Save</button>
-            </div>
-        )
-    }
+  const renderItems = (props) => {
+    const items = props;
+    let objects = Object.keys(items).map((key) => [`${key}: ${items[key]}`]);
+    let displays = objects.map((item) => {
+      return (
+        <div key={item} className="resume-item">
+          <p>{item}</p>
+        </div>
+      );
+    });
+    return (
+      <div>
+        {displays}
+        <button onClick={handleSave} className="form-btn">
+          Edit
+        </button>
+      </div>
+    );
+  };
+
+  const editItems = (props) => {
+    const items = props;
+    let objects = Object.keys(items).map((key) => (
+      <input
+        type="text"
+        defaultValue={items[key]}
+        key={key}
+        id={key}
+        onChange={(e) => handleChanges(e)}
+      />
+    ));
+    return (
+      <div>
+        {objects}
+        <button onClick={handleSave} className="form-btn">
+          Save
+        </button>
+      </div>
+    );
+  };
 
   return (
-        <div>{editing ? (renderItems(items)) : (editItems(items))} </div>
-    );
-};
+    <div className="resume-display">
+      {editing ? renderItems(items) : editItems(items)}{" "}
+    </div>
+  );
+}
 
 export default RenderOutput;
